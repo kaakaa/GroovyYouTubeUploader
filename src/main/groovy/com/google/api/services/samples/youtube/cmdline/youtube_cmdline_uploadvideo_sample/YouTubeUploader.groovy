5d592ff86645d3a44d5b1ee5f762f4d2f8da68a8
kaakaa <stooner.hoe@gmail.com>
2014-02-08 00:18:18 +0900

    change main class

---
+++ src/main/groovy/com/google/api/services/samples/youtube/cmdline/youtube_cmdline_uploadvideo_sample/YouTubeUploader.groovy
@@ -0,0 +1,7 @@
+package com.google.api.services.samples.youtube.cmdline.youtube_cmdline_uploadvideo_sample;
+
+class YouTubeUploader {
+	public static void main(args){
+		UploadVideo.main(null)
+	}
+}
